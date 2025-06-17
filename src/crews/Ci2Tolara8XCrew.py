e41f83ae4d4edc00b122ed0762c9d239c03c79a7
Taj <tajulislamj200@gmail.com>
2025-06-17 12:07:22 +0530

    unwanted script in toml file removed

--- src/crews/Ci2Tolara8XCrew.py
+++ src/crews/Ci2Tolara8XCrew.py
@@ -1,21 +1,19 @@
 import sys
-import warnings
 
 from datetime import datetime
 
-from base_crews.CiToLaraCrew import CiToLaraCrew
+from base_crews.Ci2ToLara8XCrewBase import Ci2ToLara8XCrewBase
 
-warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
 # This main file is intended to be a way for you to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
 # Replace with inputs you want to test with, it will automatically
 # interpolate any tasks and agents information
-class Ci2Tolara8XCrew():
+class Ci2ToLara8XCrew():
     def __init__(self,payload):
         """
-        Initialize the Ci2Tolara8XCrew class.
-        This class is designed to run the CiToLaraCrew with specific inputs.
+        Initialize the Ci2ToLara8XCrew class.
+        This class is designed to run the Ci2ToLara8XCrewBase with specific inputs.
         """
         self.inputs = {
             'topic': 'AI LLMs',
@@ -29,7 +27,7 @@ class Ci2Tolara8XCrew():
         Run the crew.
         """
         try:
-            CiToLaraCrew().crew().kickoff(inputs=self.inputs)
+            Ci2ToLara8XCrewBase().crew().kickoff(inputs=self.inputs)
         except Exception as e:
             raise Exception(f"An error occurred while running the crew: {e}")
 
@@ -43,7 +41,7 @@ class Ci2Tolara8XCrew():
     #         'current_year': str(datetime.now().year)
     #     }
     #     try:
-    #         CiToLaraCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+    #         Ci2ToLara8XCrewBase().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
     #     except Exception as e:
     #         raise Exception(f"An error occurred while training the crew: {e}")
@@ -53,7 +51,7 @@ class Ci2Tolara8XCrew():
     #     Replay the crew execution from a specific task.
     #     """
     #     try:
-    #         CiToLaraCrew().crew().replay(task_id=sys.argv[1])
+    #         Ci2ToLara8XCrewBase().crew().replay(task_id=sys.argv[1])
 
     #     except Exception as e:
     #         raise Exception(f"An error occurred while replaying the crew: {e}")
@@ -68,7 +66,7 @@ class Ci2Tolara8XCrew():
     #     }
         
     #     try:
-    #         CiToLaraCrew().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+    #         Ci2ToLara8XCrewBase().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
 
     #     except Exception as e:
     #         raise Exception(f"An error occurred while testing the crew: {e}")
